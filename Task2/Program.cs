a1201c3fae01663c1325c8341421351d2df9c8b7
yaroslav <yarius1979@gmail.com>
2016-10-12 10:20:42 +0300

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleLinkedLisd sll = new SingleLinkedLisd();
+            SingleLinkedLisd sll1 = new SingleLinkedLisd();
+
+            sll.Add(1);
+            sll.Add(2);
+            sll.Add(3);
+            sll.Add(4);
+            sll.Add(5);
+            sll.Add(6);
+            sll.Add(7);
+            sll.Add(8);
+
+            sll1.Add(8);
+            sll1.Add(7);
+            sll1.Add(6);
+            sll1.Add(5);
+            sll1.Add(3);
+            sll1.Add(2);
+            sll1.Add(1);
+
+            Console.WriteLine(sll.SumNodeValues() - sll1.SumNodeValues());
+        }
+    }
+}
---
+++ Task2/SingleLinkedLisd.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class SingleLinkedLisd
+    {
+        Node Tail;
+        Node Head;
+
+        int Sum;
+
+        public void Add(Node node)
+        {
+            if (node != null)
+            {
+                if (this.Head == null)
+                {
+                    Head = node;
+                    Tail = node;
+                }
+                else
+                {
+                    Tail.Next = node;
+                    Tail = node;
+                }
+            }
+        }
+
+        public void Add(int val)
+        {
+            Node node = new Node() { Value = val };
+
+            Add(node);
+        }
+
+        public int SumNodeValues()
+        {
+            return SumNodeValues(this.Head);
+        }
+
+        public int SumNodeValues(Node node)
+        {
+            if (node != null)
+            {
+                Sum += node.Value;
+                if(node.Next != null)
+                {
+                    SumNodeValues(node.Next);
+                }
+            }
+
+            return Sum;
+        }
+    }
+
+
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+}
